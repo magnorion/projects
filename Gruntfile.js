215ea77e5d8f1b9a57b6a58f10e05e5a45033a26
Lucas Dias Lopes <lucas_front@yahoo.com.br>
2015-09-04 18:24:24 -0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		sass:{
+			dist:{
+				options:{
+					style: 'expanded'
+				},
+				files:{
+					'app/css/style-theme.css':'app/sass/style-theme.scss',
+					'app/css/style-login-page.css':'app/sass/style-login-page.scss',
+					'app/css/style-forgotLogin-page.css':'app/sass/style-forgotLogin-page.scss'
+				}
+			}
+		},
+		uglify:{
+			my_target:{
+				files:{
+					'app/js/script-site.min.js':['app/js/script-site.js']
+				}
+			}
+		},
+		watch:{
+			js:{
+				files:['app/js/script-site.js'],
+				tasks:['uglify']
+			},
+			sass:{
+				files:['app/sass/*.scss'],
+				tasks:['sass']
+			}
+		},
+		browserSync:{
+			dev:{
+				bsFiles:{
+					src:[
+						'app/css/*.css',
+						'app/js/script.min.js',
+						'app/*.html'
+					]
+				},
+				options:{
+					watchTask: true,
+					server:{
+						baseDir:"./app",
+						index:"index.html"
+					}
+				}
+			}
+		},
+		copy:{
+			main:{
+				expand:true,
+				cwd: 'app/',
+				src: ['css/**','js/**','assets/**','*.html','!sass'],
+				dest:'dist'
+			}
+		},
+		clean:{
+			src: "dist/"
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+	grunt.registerTask('default', ['browserSync', 'watch']);
+	grunt.registerTask('gerarApp', ['clean', 'copy']);
+}
